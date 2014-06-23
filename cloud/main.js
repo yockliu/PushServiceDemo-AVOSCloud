752efbc9568cd1f3a6adfad1074fe9c44d9a6574
Yock <angelhunt@163.com>
2014-06-23 15:10:12 +0800

    feature: add AVOS push function

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,13 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
+});
+
+AV.Cloud.define("pushAVOS", function(request, response) {
+	AV.Push.send({
+		data: {
+			alert: "AVOS message"
+		}
+	});
 });

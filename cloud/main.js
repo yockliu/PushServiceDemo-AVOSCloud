8b6942d726f3d06e06f403f01ec6f4ff6deaac1c
Yock <angelhunt@163.com>
2014-06-26 11:30:00 +0800

    to lower case

--- cloud/main.js
+++ cloud/main.js
@@ -4,7 +4,7 @@ AV.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
 });
 
-var Push = require('cloud/BaiduPush.js');
+var Push = require('cloud/bpush.js');
 var opt = {
 	ak : "hFSfD5dCAbDHnIbANtcuSkt1",
 	sk : "6Veralrj7apiRBthfo1NW5NjUdNAapM6",

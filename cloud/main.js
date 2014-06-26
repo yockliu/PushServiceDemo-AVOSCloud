64e75320c36dddeacf77587a86298efb798e6487
Yock <angelhunt@163.com>
2014-06-26 10:55:23 +0800

    feat: 增加了百度push

--- cloud/main.js
+++ cloud/main.js
@@ -4,10 +4,40 @@ AV.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
 });
 
+var Push = require('./BaiduPush');
+var opt = {
+	ak : "hFSfD5dCAbDHnIbANtcuSkt1",
+	sk : "6Veralrj7apiRBthfo1NW5NjUdNAapM6",
+}
+var baiduClient = new Push(opt);
+function baiduPushMessage() {
+	var opt = {
+		push_type: 1,
+		messages: JSON.stringify(["hello, push0"]),
+		msg_keys: JSON.stringify(["key0"]),
+	}
+	baiduClient.pushMsg(opt, function(err, result) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log(result);
+	});
+}
+
 AV.Cloud.define("pushAVOS", function(request, response) {
 	AV.Push.send({
 		data: {
-			alert: "AVOS message"
+			alert: "AVOS Alert"
 		}
 	});
+
+	AV.Push.send({
+		data: {
+			alert: "AVOS content-available",
+			content-available: 1
+		}
+	});
+
+	baiduPushMessage();
 });
